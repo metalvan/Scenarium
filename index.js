5bbfc108141e0e6bc7e60dff5dc0a06bf951fe14
unknown <Matt@Guest1-HP.(none)>
2012-12-21 10:41:48 -0800

    new card back with proper spelling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 express = require('express')
 var app = express()
 
-/* 
-app.get('/', function(req,res){
+
+app.get('/emailsubmit', function(req,res){
 	res.send('hello world');
 });
 
-*/
+
 
 app.use(express.static(__dirname + '/public'));
 
