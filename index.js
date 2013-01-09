6c3d16e3de87a68d1be0a412f759a303da4e80b4
unknown <Matt@Guest1-HP.(none)>
2013-01-09 15:08:45 -0800

    added mailer

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-express = require('express')
+var express = require('express')
+var nodemailer = require("nodemailer");
+
 var app = express()
+app.use(express.bodyParser());
+var smtpTransport = nodemailer.createTransport("SMTP",{
+	service: "Gmail",
+	auth: {
+		user:process.env.user,
+		pass:process.env.pass
+	}
+});
 
+var sendMail = function( to, callback) {
+	smtpTransport.sendMail( {
+		from: "Scenarium < testforscenarium@gmail.com >",
+		to: to,
+		subject: "Here's Your Copy of Scenarium",
+		text: "Enjoy!",
+		html:"<b>Enjoy!!!</b>"
+	}, callback);
+}
 
-app.get('/emailsubmit', function(req,res){
-	res.send('hello world');
+app.post('/submit', function(req,res){
+	var email=(req.param('email',null));
+	sendMail(email, function(err){
+		if(err) res.send("error");
+		else res.send("ok");
+	});
 });
 
 
